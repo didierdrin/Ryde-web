493bdd4720313a8d81a2e1c8d595caf1849db710
Didier Nsengiyumva <nsedidier@gmail.com>
2025-12-19 08:14:10 +0200

    latest updates 814

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -12,11 +12,16 @@ const LandingPage = () => {
             <nav className="landing-nav">
                 <div className="nav-container">
                     <div className="nav-logo">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" fill="#3B82F6" />
-                            <path d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" fill="white" />
-                        </svg>
-                        <span className="brand-name">RYDE</span>
+                        <img 
+    src="ryde-icon.png" 
+    alt="RYDE" 
+    style={{
+        width: "24px",
+        height: "24px",
+        borderRadius: "50%",
+        objectFit: "cover"
+    }}
+/>                        <span className="brand-name">RYDE</span>
                     </div>
 
                     <div className="nav-links">
